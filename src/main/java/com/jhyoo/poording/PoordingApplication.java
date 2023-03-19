b0ab85de51ea8bd443e10d8e1d207b32454d1886
YooJaeHyeon <jhyoo1203@gmail.com>
2023-03-19 20:51:28 +0900

    initial setting

---
+++ src/main/java/com/jhyoo/poording/PoordingApplication.java
@@ -0,0 +1,13 @@
+package com.jhyoo.poording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoordingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhyoo/poording/PoordingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhyoo.poording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoordingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
